c62ab3dcc790e48a5c29d38516598361d62346be
HatelaxD <115532867+HatelaxD@users.noreply.github.com>
2022-10-18 01:10:33 +0500

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -653,9 +653,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100023592893610",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "50%3A2XFFa1QsXG5xkQ%3A2%3A1666036624%3A-1%3A6046%3A%3AAcWysWEj8AcZ_Qg6sDIiKfuF1S-eef224qUQYyh4aA"
 }
 
 
